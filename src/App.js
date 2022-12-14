dea2460f730b90777a2fa8fc26d306cb1cfff610
rezakali <rezakali1993@gmail.com>
2022-12-14 22:19:42 +0530

    Added delete functionality

--- src/App.js
+++ src/App.js
@@ -6,13 +6,18 @@ import List from "./components/List"
 
 function App() {
   const [people, setPeople ] = useState(data)
+
+  const handleDelete = (id) =>{
+    const newPeople = people.filter((pers) => pers.id !== id)
+    setPeople(newPeople)
+  }
   return (
     <>
     <Container>
       <div className="w-50 m-auto">
         <Card className="mt-5 p-3">
             <h2 className="mb-4">{people.length} Birthdays Today</h2>
-            <List people = {people} />
+            <List people = {people} handleDelete={handleDelete} />
             <Button variant="success" onClick={() => setPeople([])}>Clear All</Button>
         </Card>
       </div>
--- src/components/List.js
+++ src/components/List.js
@@ -1,4 +1,4 @@
-const List = ({ people }) => {
+const List = ({ people, handleDelete }) => {
   return (
     <>
       {people.map((person) => {
@@ -6,11 +6,11 @@ const List = ({ people }) => {
         return (
           <div key={id} className="card mb-2 p-2">
             <h4>{Name} </h4>
-            <div className="d-flex g-2">
+            <div className="d-flex g-2 mb-2">
               <span className="me-3">{age} Years</span>
               {gender === "Male" ? <p className="text-danger m-0">{gender} </p> : <p className="text-success m-0">{gender} </p> }
             </div>
-            {/* <button onClick={() => (console.log(person))} className="btn btn-danger">Delete</button> */}
+            <button onClick={() => handleDelete(id)} className="btn btn-danger">Delete</button>
           </div>
         );
       })}
